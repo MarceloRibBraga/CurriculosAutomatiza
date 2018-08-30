1b98be562a86bac3f61631102af7ba5b4da127ce
marcelo braga <marcelo@mrbsistemas.net.br>
2018-08-29 21:31:57 -0300

    Adicionar arquivos de projeto.

---
+++ src/CurriculoAutomatiza.Domain/Entity/Profissional.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurriculoAutomatiza.Domain.Entity
+{
+    public class Profissional
+    {
+        public Profissional()
+        {
+
+        }
+
+        public int ProfissionalId { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+
+
+    }
+}
---
+++ src/CurriculoAutomatiza.Infraesctructure/Data/AppContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using CurriculoAutomatiza.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurriculoAutomatiza.Infraesctructure.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options)
+        : base(options)
+        {
+                
+        }
+
+        public DbSet<Profissional> Profissionais { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Profissional>().ToTable("Profissional");
+            #region Configuraçõe de Profissional
+
+            modelBuilder.Entity<Profissional>()
+                .HasKey(p => p.ProfissionalId);
+
+            modelBuilder.Entity<Profissional>().Property(p => p.Nome)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            modelBuilder.Entity<Profissional>().Property(p => p.Cpf)
+                .HasColumnType("varchar(11)");
+
+            modelBuilder.Entity<Profissional>().Property(p => p.Email)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            modelBuilder.Entity<Profissional>().Property(p => p.Telefone)
+                .HasColumnType("varchar(20)");
+
+            modelBuilder.Entity<Profissional>().Property(p => p.Senha)
+                .HasColumnType("varchar(10)")
+                .IsRequired();
+
+            #endregion
+        }
+    }
+}
